eb1d18484c62b094d858e74e09e059923ac92e54
Haiyang <hjin317@aucklanduni.ac.nz>
2020-05-09 10:00:57 +0400

    bug fixed again...

--- simulate_data_lmm.R
+++ simulate_data_lmm.R
@@ -131,8 +131,7 @@ simudata.lmm <- function (params, usetibble = 0) {
   
   # Sum up all the components of RT
   if (usetibble) {
-    RT = RT.fix + RT.rnd.int + RT.rnd.slp
-    simudata <- tibble(expdesign, RT)
+    simudata <- tibble(expdesign, tibble(RT = RT.fix + RT.rnd.int + RT.rnd.slp))
   } else {
     simudata <- data.frame(expdesign, RT = RT.fix + RT.rnd.int + RT.rnd.slp)
   }
