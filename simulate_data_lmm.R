624388b5bd0bd4203e2edcbdaf51e1dd0f0b2530
Haiyang <hjin317@aucklanduni.ac.nz>
2020-06-05 16:48:26 +0400

    update '.' to '_' for variable names

--- simulate_data_lmm.R
+++ simulate_data_lmm.R
@@ -10,8 +10,8 @@ simudata_lmm <- function (params, usetibble = 0) {
   # source("path/to/simulate_data_lmm.R")
   # params <- list(
   #   # set the parameters for experiment design
-  #   IV1.levels = c("upright", "inverted"),  # (has to be two levels)
-  #   IV2.levels = c("intact", "scrambled"),  # (has to be two levels)
+  #   IV1_levels = c("upright", "inverted"),  # (has to be two levels)
+  #   IV2_levels = c("intact", "scrambled"),  # (has to be two levels)
   #   num_Subj = 30,
   #   num_Stim = 20,
   # 
@@ -36,7 +36,7 @@ simudata_lmm <- function (params, usetibble = 0) {
   # simudata <- simudata_lmm(params = params)
   #
   # library(lme4)
-  # lmm <- lmer(RT ~ IV1 * IV2 + (1 + IV1 * IV2|Subject) + (1 + IV1 * IV2 | Stimuli), data = simudata) # full model
+  # lmm <- lmer(Resp ~ IV1 * IV2 + (1 + IV1 * IV2|Subject) + (1 + IV1 * IV2 | Stimuli), data = simudata) # full model
   # summary(lmm)
   
   # load library
@@ -44,8 +44,8 @@ simudata_lmm <- function (params, usetibble = 0) {
   
   ## load the parameters from params
   # set the parameters for experiment design
-  IV1.levels <- params$IV1.levels
-  IV2.levels <- params$IV2.levels
+  IV1_levels <- params$IV1_levels
+  IV2_levels <- params$IV2_levels
   num_Subj <- params$num_Subj
   num_Stim <- params$num_Stim
   
@@ -68,13 +68,13 @@ simudata_lmm <- function (params, usetibble = 0) {
   
 
   # number of levels for IV1 and IV2
-  numlevel_IV1 <- length(IV1.levels)
-  numlevel_IV2 = length(IV2.levels)
+  numlevel_IV1 <- length(IV1_levels)
+  numlevel_IV2 = length(IV2_levels)
   num_total = numlevel_IV1 * numlevel_IV2 * num_Stim * num_Subj
   
   # generate factors for all non-dependent variables
-  IV1 <- gl(n = numlevel_IV1, k = 1, length = num_total, labels = IV1.levels)
-  IV2 <- gl(n = numlevel_IV2, k = numlevel_IV1, length = num_total, labels = IV2.levels)
+  IV1 <- gl(n = numlevel_IV1, k = 1, length = num_total, labels = IV1_levels)
+  IV2 <- gl(n = numlevel_IV2, k = numlevel_IV1, length = num_total, labels = IV2_levels)
   Stim <- gl(n = num_Stim, k = numlevel_IV1 * numlevel_IV2, length = num_total, labels = paste0("stim", 1:num_Stim))
   Subj <- gl(n = num_Subj, k = numlevel_IV1 * numlevel_IV2 * num_Stim, length = num_total, labels = paste0("P", 1:num_Subj))
   
@@ -86,25 +86,25 @@ simudata_lmm <- function (params, usetibble = 0) {
   
   # str(expdesign)
   
-  # RT for fixed factors
-  modmat.fix <- model.matrix( ~ IV1 * IV2, expdesign)
+  # Resp for fixed factors
+  modmat_fix <- model.matrix( ~ IV1 * IV2, expdesign)
   
   # calculate the betas
   intercept = IV1.1_IV2.1
   int_IV1.2 = IV1.2_IV2.1 - intercept
   slp_IV2.2 = (IV1.1_IV2.2 - intercept) / 1  # divided by 1 (dummy coding)
   interaction = (IV1.2_IV2.2 - IV1.1_IV2.2) - int_IV1.2
-  coeff.fix <- c(intercept, int_IV1.2, slp_IV2.2, interaction)
+  coeff_fix <- c(intercept, int_IV1.2, slp_IV2.2, interaction)
   
-  # added random variance (i.e. the residual) to the RT of fixed effect
-  RT.fix <- rnorm(n = num_total, mean = modmat.fix %*% coeff.fix, sd = var_residual)
+  # added random variance (i.e. the residual) to the Resp of fixed effect
+  Resp_fix <- rnorm(n = num_total, mean = modmat_fix %*% coeff_fix, sd = var_residual)
   
-  # Random intercepts of RT for every subject or stimulus
+  # Random intercepts of Resp for every subject or stimulus
   rnd_int_subj <- rnorm(num_Subj, 0, var_rnd_int_subj)  # Subject
   rnd_int_stim <- rnorm(num_Stim, 0, var_rnd_int_stim)  # Stimulus
   
   # The sum of the random intercepts of Subjects and Stimuli
-  RT.rnd.int <- rnd_int_subj[as.numeric(Subj)] +  rnd_int_stim[as.numeric(Stim)]
+  RT_rnd_int <- rnd_int_subj[as.numeric(Subj)] +  rnd_int_stim[as.numeric(Stim)]
   
   # RT for random slopes
   rnd_slp_IV1_subj <- rnorm(num_Subj, 0, var_rnd_slp_IV1_subj)
@@ -115,35 +115,35 @@ simudata_lmm <- function (params, usetibble = 0) {
   rnd_slp_IV2_stim <- rnorm(num_Stim, 0, var_rnd_slp_IV2_stim)
   rnd_slp_inter_stim <- rnorm(num_Stim, 0, var_rnd_slp_inter_stim)
   
-  IV1_2 <- paste0("IV1", IV1.levels[2])  # column name
-  IV2_2 <- paste0("IV2", IV2.levels[2])  # column name
+  IV1_2 <- paste0("IV1", IV1_levels[2])  # column name
+  IV2_2 <- paste0("IV2", IV2_levels[2])  # column name
   
-  IV1.dummy <- modmat.fix[, IV1_2]
-  IV2.dummy <- modmat.fix[, IV2_2]
-  inter.dummy <- modmat.fix[, paste(IV1_2, IV2_2, sep = ":")]
+  IV1_dummy <- modmat_fix[, IV1_2]
+  IV2_dummy <- modmat_fix[, IV2_2]
+  inter_dummy <- modmat_fix[, paste(IV1_2, IV2_2, sep = ":")]
   
-  RT.rnd.slp <- rnd_slp_IV1_subj[as.numeric(Subj)] * IV1.dummy +
-    rnd_slp_IV2_subj[as.numeric(Subj)] * IV2.dummy +
-    rnd_slp_inter_subj[as.numeric(Subj)] * inter.dummy +
-    rnd_slp_IV1_stim[as.numeric(Stim)] * IV1.dummy +
-    rnd_slp_IV2_stim[as.numeric(Stim)] * IV2.dummy +
-    rnd_slp_inter_stim[as.numeric(Stim)] * inter.dummy
+  RT_rnd_slp <- rnd_slp_IV1_subj[as.numeric(Subj)] * IV1_dummy +
+    rnd_slp_IV2_subj[as.numeric(Subj)] * IV2_dummy +
+    rnd_slp_inter_subj[as.numeric(Subj)] * inter_dummy +
+    rnd_slp_IV1_stim[as.numeric(Stim)] * IV1_dummy +
+    rnd_slp_IV2_stim[as.numeric(Stim)] * IV2_dummy +
+    rnd_slp_inter_stim[as.numeric(Stim)] * inter_dummy
   
   # Sum up all the components of RT
   if (usetibble) {
-    simudata <- add_column(expdesign, RT = RT.fix + RT.rnd.int + RT.rnd.slp)
+    simudata <- add_column(expdesign, RT = RT_fix + RT_rnd_int + RT_rnd_slp)
   } else {
-    simudata <- data.frame(expdesign, RT = RT.fix + RT.rnd.int + RT.rnd.slp)
+    simudata <- data.frame(expdesign, RT = RT_fix + RT_rnd_int + RT_rnd_slp)
   }
   return(simudata)
 }
 
 
 # function to compare the results of fitted model to the pre-set parameters
-compare.simu <- function(params, fit, isconfint = FALSE) {
+compare_simu <- function(params, fit, isconfint = FALSE) {
   # author: Haiyang Jin (https://haiyangjin.github.io/)
-  # Usage: compare.lmm(params, fit)$fixed
-  #        compare.lmm(params, fit)$random
+  # Usage: compare_simu(params, fit)$fixed
+  #        compare_simu(params, fit)$random
   
   # load library
   library(magrittr)  # pipe
@@ -173,34 +173,34 @@ compare.simu <- function(params, fit, isconfint = FALSE) {
   int_IV1.2 = IV1.2_IV2.1 - intercept
   slp_IV2.2 = (IV1.1_IV2.2 - intercept) / 1  # divided by 1 (dummy coding)
   interaction = (IV1.2_IV2.2 - IV1.1_IV2.2) - int_IV1.2
-  coeff.fix <- c(intercept, int_IV1.2, slp_IV2.2, interaction)
+  coeff_fix <- c(intercept, int_IV1.2, slp_IV2.2, interaction)
   
   ## Preparation for random effects
-  IV1.levels <- params$IV1.levels
-  IV2.levels <- params$IV2.levels
-  IV1_2 <- paste0("IV1", IV1.levels[2])  # column name
-  IV2_2 <- paste0("IV2", IV2.levels[2])  # column name
+  IV1_levels <- params$IV1_levels
+  IV2_levels <- params$IV2_levels
+  IV1_2 <- paste0("IV1", IV1_levels[2])  # column name
+  IV2_2 <- paste0("IV2", IV2_levels[2])  # column name
   
-  full.var.df <- data.frame(
+  full_var_df <- data.frame(
     grp = c(rep("Stimuli", 4), rep("Subject", 4), "Residual"),
     var1 = c(rep(c("(Intercept)", IV1_2, IV2_2, paste(IV1_2, IV2_2, sep = ":")), 2), NA)
   )
   
-  rnd.var <- {
+  rnd_var <- {
     as.data.frame(VarCorr(fit)) %>% 
       filter(is.na(var2)) %>% 
       select(grp, var1, sdcor) %>% 
       rename(sd = sdcor)
   }
   
-  rnd.var.full <- merge(full.var.df, rnd.var, all = TRUE)
+  rnd_var_full <- merge(full_var_df, rnd_var, all = TRUE)
   
-  assumed.variance <- c(var_residual,
+  assumed_variance <- c(var_residual,
                         var_rnd_int_stim, var_rnd_slp_IV1_stim, var_rnd_slp_inter_stim, var_rnd_slp_IV2_stim, 
                         var_rnd_int_subj, var_rnd_slp_IV1_subj, var_rnd_slp_inter_subj, var_rnd_slp_IV2_subj
   )
   
-  compare.rnd <- data.frame(rnd.var.full, assumed.variance)
+  compare_rnd <- data.frame(rnd_var_full, assumed_variance)
   
   
   # add the confident intervals if isconfint == TURE
@@ -208,29 +208,29 @@ compare.simu <- function(params, fit, isconfint = FALSE) {
     # load library
     library(lme4)
     # confidence intervals for fixed effects
-    confint.fix <- as.data.frame(confint.merMod(fit, parm = "beta_", oldNames = FALSE))  # This step takes a little bit longer
+    confint_fix <- as.data.frame(confint.merMod(fit, parm = "beta_", oldNames = FALSE))  # This step takes a little bit longer
     # confidence intervals for random effects
-    confint.rnd <- as.data.frame(confint.merMod(fit, parm = "theta_", oldNames = FALSE))  # This step takes too long
+    confint_rnd <- as.data.frame(confint.merMod(fit, parm = "theta_", oldNames = FALSE))  # This step takes too long
     
     # compare fixed
-    compare.fixed <- cbind(as.data.frame(fixef(fit)), confint.fix, assumed.coeff = coeff.fix)
+    compare_fixed <- cbind(as.data.frame(fixef(fit)), confint_fix, assumed_coeff = coeff_fix)
     # compare random
-    compare.rnd <- cbind(rnd.var.full, confint.rnd, assumed.variance)
+    compare_rnd <- cbind(rnd_var_full, confint_rnd, assumed_variance)
     
   } else {
     # compare fixed
-    compare.fixed <- cbind(as.data.frame(fixef(fit)), assumed.coeff = coeff.fix)
+    compare_fixed <- cbind(as.data.frame(fixef(fit)), assumed_coeff = coeff_fix)
     # compare random
-    compare.rnd <- cbind(rnd.var.full, assumed.variance)
+    compare_rnd <- cbind(rnd_var_full, assumed_variance)
     
   }
   
-  compare.lmm <- list(
-    fixed = compare.fixed,
-    random = compare.rnd
+  compare_lmm <- list(
+    fixed = compare_fixed,
+    random = compare_rnd
   )
   
-  return(compare.lmm)  
+  return(compare_lmm)  
 }
 
 
